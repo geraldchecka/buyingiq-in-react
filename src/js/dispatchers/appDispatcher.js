de9c7456be9f63dc51b21b1219ef4ddc6d3ac8cb
geraldchecka <anvesh.checka@gmail.com>
2015-12-13 00:32:05 +0530

    file cleanup

--- src/js/dispatchers/appDispatcher.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Dispatcher } from 'flux';
-import 
-
-console.log(Dispatcher);
-
-module.exports = AppDispatcher;
--- src/js/dispatchers/storeDispatcher.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Dispatcher } from 'flux';
-
-var lol = new Dispatcher();
-
-// NetDispatcher
-module.exports = {};l
